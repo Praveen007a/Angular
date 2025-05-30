83feb974af3c513cd376b405c26ca901ff1c87a5
Praveen007a <praveendhamodharncs2020@gmail.com>
2025-05-30 10:53:35 +0530

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'MyApp';
+  public desccription = 'This is a sample Angular application.';
+  fullName = "Praveen D";
+
+  getName(){
+    return "TP";
+  }
+
+  imgUrl = "https://picsum.photos/1200/500";
+  imgAlt = "Demo Image";
+
+  isDisabled = false;
+
+  clickMe(){
+    alert("You clicked!");
+    window.location.reload();
+  }
+  // Counter App
+  value =1;
+
+  increment(){
+    this.value++;
+  }
+  reset(){
+    this.value = 0;
+  }
+  decrement(){
+    this.value--;
+    if(this.value < 0){
+      this.value = 0;
+    }
+  }
+  
+
+
 }
